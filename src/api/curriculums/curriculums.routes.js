76cd7aa18fc8343992c12de1ec04b16b31dad64b
Neftali Cruz <110734357+NeftaliCruzSosa@users.noreply.github.com>
2022-11-06 20:50:33 +0100

    Feat: Add new routes to use logged user data as default ID

--- src/api/curriculums/curriculums.routes.js
+++ src/api/curriculums/curriculums.routes.js
@@ -6,7 +6,21 @@ const { isAuth, isAdmin } = require("../../middlewares/auth");
 const upload = require("../../middlewares/file");
 const deleteFile = require("../../middlewares/deleteFile");
 
-router.get("/:id", async (req, res, next) => {
+//* Get logged user curriculum
+router.get("/", [isAuth], async (req, res, next) => {
+  try {
+    const userID = req.user._id.toString();
+    const oldUser = await User.findById(userID).populate("curriculum");
+    const curriculumId = oldUser.curriculum._id.toHexString();
+    const curriculumToFind = await Curriculum.findById(curriculumId);
+    return res.status(200).json(curriculumToFind);
+  } catch (error) {
+    return next(error);
+  }
+});
+
+//* Get curriculum by ID
+router.get("/byId/:id", async (req, res, next) => {
   try {
     const id = req.params.id;
     const curriculumToFind = await Curriculum.findById(id);
@@ -16,7 +30,7 @@ router.get("/:id", async (req, res, next) => {
   }
 });
 
-
+//* Create new curriculum, if user has an existent curriculum, old curriculum is deleted and new curriculum ID is added to user
 router.post("/create", [isAuth], upload.single("img"), async (req, res, next) => {
   try {
     const curriculum = req.body;
@@ -25,14 +39,14 @@ router.post("/create", [isAuth], upload.single("img"), async (req, res, next) =>
     }
     const newCurriculum = new Curriculum(curriculum);
     const curriculumCreated = await newCurriculum.save();
-    const userID = req.user._id.toString()
-    const oldUser = await User.findById(userID)
-    if(oldUser.curriculum){
-        const oldCurriculum = oldUser.curriculum.toHexString()
-        const deleted = await Curriculum.findByIdAndDelete(oldCurriculum);
-        console.log(deleted)
+    const userID = req.user._id.toString();
+    const oldUser = await User.findById(userID);
+    if (oldUser.curriculum) {
+      const oldCurriculum = oldUser.curriculum.toHexString();
+      const deleted = await Curriculum.findByIdAndDelete(oldCurriculum);
+      console.log(deleted);
     }
-    oldUser.curriculum = curriculumCreated
+    oldUser.curriculum = curriculumCreated;
     const userModify = new User(oldUser);
     userModify._id = userID;
     await User.findByIdAndUpdate(userID, userModify);
@@ -42,7 +56,41 @@ router.post("/create", [isAuth], upload.single("img"), async (req, res, next) =>
   }
 });
 
-router.delete("/delete/:id", [isAdmin], async (req, res, next) => {
+//* Edit user logged curriculum
+router.put("/edit/", [isAuth], upload.single("img"), async (req, res, next) => {
+  try {
+    const userID = req.user._id.toString();
+    const oldUser = await User.findById(userID).populate("curriculum");
+    const oldCurriculumId = oldUser.curriculum._id.toString();
+    const curriculum = req.body;
+    const curriculumModify = new Curriculum(curriculum);
+    curriculumModify._id = oldCurriculumId;
+    await Curriculum.findByIdAndUpdate(oldCurriculumId, curriculumModify);
+    return res.status(200).json(`Modified --> ${curriculumModify}`);
+  } catch (error) {
+    return next(error);
+  }
+});
+
+//* Delete user logged curriculum
+router.delete("/delete", [isAuth], async (req, res, next) => {
+    try {
+        const userID = req.user._id.toString();
+        const oldUser = await User.findById(userID).populate("curriculum");
+        const oldCurriculumId = oldUser.curriculum._id.toString();
+    //   const curriculum = await Curriculum.findById(id);
+    //   if (curriculum.profilePic) {
+    //     deleteFile(curriculum.profilePic);
+    //   }
+      const curriculumToDelete = await Curriculum.findByIdAndDelete(oldCurriculumId);
+      return res.status(200).json(`Deleted --> ${curriculumToDelete.name}`);
+    } catch (error) {
+      return next(error);
+    }
+  });
+
+//* Delete curriculum by ID, only admin allowed
+router.delete("/deleteById/:id", [isAdmin], async (req, res, next) => {
   try {
     const id = req.params.id;
     const curriculum = await Curriculum.findById(id);
@@ -51,19 +99,6 @@ router.delete("/delete/:id", [isAdmin], async (req, res, next) => {
     }
     const curriculumToDelete = await Curriculum.findByIdAndDelete(id);
     return res.status(200).json(`Deleted --> ${curriculumToDelete.name}`);
-  } catch (error) {
-    return next(error);
-  }
-});
-
-router.put("/edit/:id", [isAdmin], upload.single("img"), async (req, res, next) => {
-  try {
-    const id = req.params.id;
-    const curriculum = req.body;
-    const curriculumModify = new Curriculum(curriculum);
-    curriculumModify._id = id;
-    await Curriculum.findByIdAndUpdate(id, curriculumModify);
-    return res.status(200).json("Modified");
   } catch (error) {
     return next(error);
   }
