9cf4e2b22f4dbfd4a382a38d14a6970b01a76708
Neftali Cruz <110734357+NeftaliCruzSosa@users.noreply.github.com>
2022-11-07 13:27:13 +0100

    Error found

--- src/middlewares/deletefile.js
+++ src/middlewares/deletefile.js
@@ -1,18 +1,14 @@
 const cloudinary = require("cloudinary").v2;
 
 const deleteFile = (imgUrl) => {
+  const imgSplited = imgUrl.split("/");
+  const nameSplited = imgSplited[imgSplited.length - 1].split(".")[0];
+  const folderSplited = imgSplited[imgSplited.length - 2];
+  const public_id = `${folderSplited}/${nameSplited}`;
 
-    const imgSplited = imgUrl.split("/");
-    const nameSplited = imgSplited[imgSplited.length - 1].split(".")[0];
-    const folderSplited = imgSplited[imgSplited.length - 2];
-    const public_id = `${folderSplited}/${nameSplited}`;
+  cloudinary.uploader.destroy(public_id, () => {
+    console.log("File deleted");
+  });
+};
 
-    cloudinary.uploader.destroy(public_id, () => {
-
-        console.log("File deleted");
-
-    })
-    
-}
-
-module.exports = deleteFile
+module.exports = deleteFile;
